8ce5de2524d6ab003d1bbe4af607ac2b1b9a9658
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-04-08 21:07:54 -0500

    45. Interpolación, estructura HTML y estilos

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -5,4 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css',
 })
-export class HeroComponent {}
+export class HeroComponent {
+  public name: string = 'ironman';
+  public age: number = 45;
+}
