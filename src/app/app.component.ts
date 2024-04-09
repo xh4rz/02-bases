081350cbae0a7e7e45cb4db5b396c46817b5ba18
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-04-08 20:28:00 -0500

    43. Funcionalidad del contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title: string = 'Hola Mundo';
-  public counter: number = 10;
-
-  increaseBy(value: number): void {
-    this.counter += value;
-  }
-
-  resetCounter() {
-    this.counter = 10;
-  }
 }
