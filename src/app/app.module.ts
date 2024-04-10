997b7507af9b3c5b628312205b7abd94cb92c8c0
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-04-10 01:07:54 -0500

    51. Módulos en Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { CounterModule } from './counter/counter.module';
+
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
 import { HeroComponent } from './heroes/hero/hero.component';
 import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
-  declarations: [AppComponent, CounterComponent, HeroComponent, ListComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, HeroComponent, ListComponent],
+  imports: [BrowserModule, CounterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
