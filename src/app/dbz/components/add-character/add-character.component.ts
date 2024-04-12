f0378c8db5cf54b0244815c96466b51d8ef57b82
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-04-12 16:02:21 -0500

    67. @Output() - Emitir eventos al padre

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -7,6 +7,9 @@ import { Character } from '../../interfaces/character.interface';
   styleUrl: './add-character.component.css',
 })
 export class AddCharacterComponent {
+  @Output()
+  public onNewCharacter: EventEmitter<Character> = new EventEmitter();
+
   public character: Character = {
     name: '',
     power: 0,
@@ -14,5 +17,12 @@ export class AddCharacterComponent {
 
   emitCharacter(): void {
     console.log(this.character);
+
+    if (this.character.name.length === 0) return;
+
+    this.onNewCharacter.emit(this.character);
+
+    this.character.name = '';
+    this.character.power = 0;
   }
 }
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -20,4 +20,8 @@ export class MainPageComponent {
       power: 7500,
     },
   ];
+
+  onNewCharacter(character: Character): void {
+    console.log(character);
+  }
 }
