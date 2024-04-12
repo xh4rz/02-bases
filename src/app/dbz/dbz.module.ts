c446b794d42c2058666d1666dc8030c705bbfad2
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-04-12 02:52:54 -0500

    62. Resolución de la tarea - AddCharacter

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
 
 @NgModule({
   exports: [MainPageComponent],
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, ListComponent, AddCharacterComponent],
   imports: [CommonModule],
 })
 export class DbzModule {}
