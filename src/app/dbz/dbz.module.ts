0234c3cc7584d4739a80b88e371e8ae3154ddbde
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-04-12 02:12:57 -0500

    58. Módulo DBZ (Dragon Ball Z)

---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainPageComponent } from './pages/main-page.component';
+
+@NgModule({
+  exports: [MainPageComponent],
+  declarations: [MainPageComponent],
+  imports: [CommonModule],
+})
+export class DbzModule {}
