35374e8ed9a6b17e33fe3a8ae73b7763acc96611
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-04-14 00:39:29 -0500

    72. Paquetes externos - UUID

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -16,10 +16,11 @@ export class ListComponent {
   ];
 
   @Output()
-  public onDelete: EventEmitter<number> = new EventEmitter();
+  public onDelete: EventEmitter<string> = new EventEmitter();
 
-  onDeleteCharacter(index: number): void {
-    // TODO: emitir el ID del personaje
-    this.onDelete.emit(index);
+  onDeleteCharacter(id?: string): void {
+    if (!id) return;
+
+    this.onDelete.emit(id);
   }
 }
